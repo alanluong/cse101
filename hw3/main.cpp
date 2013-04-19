641b3ca733048076f4fa195813ed22d7843a8937
Alan Luong <mcluong@acs-cseb240-19.ucsd.edu>
2013-04-19 15:24:57 -0700

    added trials

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <ctime>
 
 void bubbleSort(std::vector<int> &v) {
 	bool done = false;
@@ -26,8 +27,12 @@ void bubbleSort(std::vector<int> &v) {
 
 int main(int argc, char* argv[])
 {
-	std::vector<int> v;
 	bool useHeap = false;
+
+	std::cout << argv[2] << std::endl;
+	if (std::string(argv[2]) == "heap") {
+		useHeap = true;
+	}
 
 	std::string num(argv[1]);
 	std::stringstream ss(num);
@@ -36,33 +41,33 @@ int main(int argc, char* argv[])
 	int range = 2;
 
 	for (int i = 0; i < n - 1; ++i) {
-		 range *= 2;
-	}
-	std::cout << range << std::endl;
-	
-	std::cout << argv[2] << std::endl;
-	if (std::string(argv[2]) == "heap") {
-		useHeap = true;
+		range *= 2;
 	}
 
-	for (int i = 0; i < range; ++i) {
-		v.push_back(1 + rand());
-	}
+	clock_t t = clock();
+	for (int trials = 0; trials < 100; ++trials) {
+		std::vector<int> v;
 
-	if (useHeap) {
-		std::cout << "heapSort" << std::endl;
-		std::make_heap(v.begin(), v.end());
-		std::sort_heap(v.begin(), v.end());
-	} else {
-		std::cout << "bubbleSort" << std::endl;
-		bubbleSort(v);
+		for (int i = 0; i < range; ++i) {
+			v.push_back(1 + rand());
+		}
+
+		if (useHeap) {
+			std::make_heap(v.begin(), v.end());
+			std::sort_heap(v.begin(), v.end());
+		} else {
+			bubbleSort(v);
+		}
 	}
+	t = clock() - t;
+	//std::cout << t / (float) CLOCKS_PER_SEC << std::endl;
+	std::cout << range << "\t" << t / (float) CLOCKS_PER_SEC << std::endl;
 
 	/*
-	for (int i = 0; i < v.size(); ++i) {
-		std::cout << v[i] << std::endl;
-	}
-	*/
+	   for (int i = 0; i < v.size(); ++i) {
+	   std::cout << v[i] << std::endl;
+	   }
+	   */
 
 	return 0;
 }
